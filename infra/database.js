8625eba47ebee806556e718ebd23e46d0ee09930
Yuri Gomes <62670285+Yuuri-gomes@users.noreply.github.com>
2024-02-09 21:55:52 +0000

    add `ssl` verification on production database connection

--- infra/database.js
+++ infra/database.js
@@ -13,6 +13,7 @@ function databaseAccessConfiguration() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV !== "development",
   };
   return databaseObjectConfiguration;
 }
