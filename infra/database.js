689706dbcde52fbef80bc680153503f0ac16dee6
Yuri Gomes <62670285+Yuuri-gomes@users.noreply.github.com>
2024-03-29 17:36:40 +0000

    improving tests coverage and config defaults on `jest.config.js`

--- infra/database.js
+++ infra/database.js
@@ -36,7 +36,7 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  return process.env.NODE_ENV !== "development";
+  return process.env.NODE_ENV === "production";
 }
 
 export default {
